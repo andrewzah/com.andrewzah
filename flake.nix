09ae9c5ee5e07bc4a9d11bc438a9d0ac18615191
Andrew Zah <zah@andrewzah.com>
2025-07-23 19:58:35 +0900

    nix: bump deps
    Signed-off-by: Andrew Zah <zah@andrewzah.com>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Builds a Quartz website and Docker image.";
+  description = "hugo website and oci container";
   outputs = {
     self,
     nixpkgs,
@@ -10,31 +10,18 @@
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
         n2c = nix2container.packages."${system}".nix2container;
-
-        hugo_145 = pkgs.hugo.overrideAttrs (prevAttrs: rec {
-          version = "0.145.0";
-
-          src = pkgs.fetchFromGitHub {
-            owner = "gohugoio";
-            repo = "hugo";
-            tag = "v${version}";
-            hash = "sha256-5SV6VzNWGnFQBD0fBugS5kKXECvV1ZE7sk7SwJCMbqY=";
-          };
-
-          vendorHash = "sha256-aynhBko6ecYyyMG9XO5315kLerWDFZ6V8LQ/WIkvC70=";
-        });
       in {
         packages = rec {
           website = pkgs.stdenv.mkDerivation {
             name = "andrewzah-hugo-website";
             src = ./.;
-            nativeBuildInputs = (with pkgs; [git hugo_145 go]);
+            nativeBuildInputs = with pkgs; [git hugo go];
 
             buildPhase = let
               hugoVendor = pkgs.stdenv.mkDerivation {
                 name = "andrewzah-hugo-website-vendor";
                 src = ./.;
-                nativeBuildInputs = (with pkgs; [ go git hugo_145 ]);
+                nativeBuildInputs = with pkgs; [go git hugo];
 
                 buildPhase = ''
                   cd src
@@ -70,39 +57,37 @@
 
             copyToRoot = let
               caddyfile = pkgs.writeTextDir "/etc/caddy/Caddyfile" (builtins.readFile ./Caddyfile);
-            in pkgs.buildEnv {
-              name = "img-root";
-              paths = [caddyfile website];
-              pathsToLink = ["/bin" "/etc" "/var"];
-            };
+            in
+              pkgs.buildEnv {
+                name = "img-root";
+                paths = [caddyfile website];
+                pathsToLink = ["/bin" "/etc" "/var"];
+              };
 
             config = {
-              Cmd = [ "${pkgs.caddy}/bin/caddy" "run" "--config" "/etc/caddy/Caddyfile" "--adapter"  "caddyfile" ];
-              ExposedPorts = { "2020" = {}; };
+              Cmd = ["${pkgs.caddy}/bin/caddy" "run" "--config" "/etc/caddy/Caddyfile" "--adapter" "caddyfile"];
+              ExposedPorts = {"2020" = {};};
             };
           };
         };
 
         devShells.default = pkgs.mkShell {
-          packages = (with pkgs; [
-            hugo_145
+          packages = with pkgs; [
+            hugo
             go
 
             awscli2
             typos
             lychee # link checker
-          ]);
-          shellHook = ''
-            alias v="nvim";
-            alias j="just";
-          '';
+          ];
         };
       }
     );
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
 
     nix2container.url = "github:nlewo/nix2container";
     nix2container.inputs.nixpkgs.follows = "nixpkgs";
